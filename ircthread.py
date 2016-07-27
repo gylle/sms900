f4187d93f2400a01692554993f8839e15502ae76
David Hedberg <david.hedberg@gmail.com>
2016-07-27 13:59:12 +0200

    Add simple check for connection aliveness

--- ircthread.py
+++ ircthread.py
@@ -14,6 +14,10 @@ class IRCThreadCallbackHandler(DefaultCommandHandler):
     def set_sms900(cls, sms900):
         cls.sms900 = sms900
 
+    @classmethod
+    def set_pong_queue(cls, pong_queue):
+        cls.pong_queue = pong_queue
+
     def __init__(self, client):
         super(IRCThreadCallbackHandler, self).__init__(client)
         self.cli = client
@@ -37,6 +41,14 @@ class IRCThreadCallbackHandler(DefaultCommandHandler):
         if m:
             args = m.group(2)
             cmd_dispatch[m.group(1).strip()](hostmask, chan, args)
+
+    def ping(self, prefix, server):
+        logging.info("PING/%s/%s" % (prefix, server))
+        self.client.send("PONG", server)
+
+    def pong(self, prefix, server, something):
+        logging.info("PONG/%s/%s/%s" % (prefix, server, something))
+        self.pong_queue.append(server)
 
     def handle_cmd_stats(self, hostmask, chan, cmd):
         self._add_event('SHOW_STATS', {
@@ -116,6 +128,9 @@ class IRCThreadCallbackHandler(DefaultCommandHandler):
         self.sms900.add_event(event)
 
 class IRCThread(Thread):
+    PING_INTERVAL = 60
+    PING_TIMEOUT = 180
+
     def __init__(self, sms900, host, port, nick, channel):
         Thread.__init__(self)
         self.irc_host = host
@@ -123,29 +138,34 @@ class IRCThread(Thread):
         self.irc_nick = nick
         self.irc_channel = channel
 
+        self.pong_queue = deque()
         self.cmd_queue = deque()
 
         IRCThreadCallbackHandler.set_sms900(sms900)
+        IRCThreadCallbackHandler.set_pong_queue(self.pong_queue)
 
     def run(self):
         while True:
             try:
                 self.connect_and_run()
             except Exception as e:
-                logging.info("Caught an exception, will try to reconnect!")
-                self.cli = None
+                logging.info("Exception caught, reconnecting: %s" % e)
 
             logging.info("Sleeping for 5 seconds..")
             time.sleep(5)
 
     def connect_and_run(self):
-        self.cli = IRCClient(IRCThreadCallbackHandler,
-                             host=self.irc_host,
-                             port=self.irc_port,
-                             nick=self.irc_nick,
-                             connect_cb=self.connect_callback)
+        cli = IRCClient(IRCThreadCallbackHandler,
+                        host=self.irc_host,
+                        port=self.irc_port,
+                        nick=self.irc_nick,
+                        connect_cb=self.connect_callback)
 
-        conn = self.cli.connect()
+        self.ping_last_reply = time.time()
+        self.ping_sent_at = False
+        self.pong_queue.clear()
+
+        conn = cli.connect()
         while True:
             next(conn)
             while len(self.cmd_queue) > 0:
@@ -153,9 +173,30 @@ class IRCThread(Thread):
                 target = cmd[1]
                 msg = cmd[2]
                 logging.info('Handling event (%s) -> (%s, %s)' % (cmd, target, msg))
-                helpers.msg(self.cli, target, msg)
+                helpers.msg(cli, target, msg)
+
+            self.check_connection(cli)
 
             time.sleep(0.2)
+
+    def check_connection(self, cli):
+        if not self.ping_sent_at:
+            if time.time() - self.ping_last_reply > self.PING_INTERVAL:
+                self.ping_sent_at = time.time()
+                cli.send("PING %s" % int(self.ping_sent_at))
+        else:
+            lag = time.time() - self.ping_sent_at
+
+            # We'll just assume that any pong received is good enough
+            if len(self.pong_queue) > 0:
+                logging.info("Current lag: %s" % lag)
+
+                self.ping_sent_at = None
+                self.ping_last_reply = time.time()
+                self.pong_queue.clear()
+            else:
+                if lag > self.PING_TIMEOUT:
+                    raise Exception("Lag is %s, exceeded timeout %s" % (lag, self.PING_TIMEOUT))
 
     def connect_callback(self, cli):
         helpers.join(cli, self.irc_channel)
